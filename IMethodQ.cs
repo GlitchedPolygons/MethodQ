fc0711020aab5df86caa767e79dc8bb7a2f3b1aa
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-02-06 01:12:26 +0100

    Defined IMethodQ interface

---
+++ IMethodQ.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodQ
+{
+    /// <summary>
+    /// Service interface for scheduling method calls using a method queue.
+    /// </summary>
+    public interface IMethodQ
+    {
+        /// <summary>
+        /// Schedules the specified action to be executed ONCE at the specified <paramref name="executionUtc"/> <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> to schedule.</param>
+        /// <param name="executionUtc">The <see cref="DateTime"/> in UTC of when the <paramref name="action"/> should be invoked.</param>
+        /// <returns>The scheduled method call's unique identifier (needed for cancellation).</returns>
+        ulong Schedule(Action action, DateTime executionUtc);
+
+        /// <summary>
+        /// Schedules the specified <see cref="Action"/> to be executed every <paramref name="interval"/> until manual cancellation.
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> to schedule.</param>
+        /// <param name="interval">Repetition interval <see cref="TimeSpan"/>.</param>
+        /// <returns>The scheduled method call's unique identifier (needed for cancellation).</returns>
+        ulong Schedule(Action action, TimeSpan interval);
+
+        /// <summary>
+        /// Cancels the specified method call (removes it from the list of scheduled calls). Irreversible!
+        /// </summary>
+        /// <param name="id">The scheduled action's identifier.</param>
+        /// <returns>Whether the cancellation was successful or not (e.g. can't double-cancel or cancel an inexistent call).</returns>
+        bool Cancel(ulong id);
+    }
+}
---
+++ MethodQ.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MethodQ
+{
+    public class MethodQ : IMethodQ
+    {
+        public ulong Schedule(Action action, DateTime executionUtc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong Schedule(Action action, TimeSpan interval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Cancel(ulong id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
